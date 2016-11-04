1e9af229ed77ba7083d50dd6ec3e88a54a925703
Don Bennett <dpb@donbennett.org>
2016-11-03 22:31:47 -0700

    Remove configs for castle > 1204

--- puppet/puppet-1404/modules/system-castle/files/usr/local/bin/backup-rusa.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-BASE=/mnt/backup
-
-rsync -av --rsh="sshpass -p xmogVj4b ssh" rusa@www.rusa.org:/usr/local/bin/ $BASE/daily.0/www.rusa.org/usr/local/bin
-rsync -av --rsh="sshpass -p xmogVj4b ssh" rusa@www.rusa.org:/var/www/ $BASE/daily.0/www.rusa.org/var/www
-rsync -av --rsh="sshpass -p xmogVj4b ssh" rusa@www.rusa.org:/etc/ $BASE/daily.0/www.rusa.org/etc
-
