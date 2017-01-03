dd58c6cfd15517f5aba88a22dbd81b3ed908df31
Don Bennett <dpb@donbennett.org>
2017-01-03 00:04:48 -0800

    Add x1 backup script

---
+++ bin/x1backup.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+BASE=/media/dpb/Backup
+
+function do_backup() {
+    NAME=$1
+
+    if [ ! -d $BASE/$NAME/current ]; then
+        /bin/btrfs subvolume create $BASE/$NAME
+        /bin/btrfs subvolume create $BASE/$NAME/current
+        mkdir $BASE/$NAME/updates
+    fi
+
+    rsync -av --delete-after /space/$NAME/ $BASE/$NAME/current > /tmp/filelist.$$
+
+    updates=`cat /tmp/filelist.$$ | sed '1d' | head -n -3 | grep -v '/$' | wc -l`
+
+    if [ $updates -gt 0 ]; then
+        backup_name="`date +%Y-%m-%dT%H:%M`"
+        /bin/btrfs subvolume snapshot -r $BASE/$NAME/current $BASE/$NAME/$backup_name
+        cp /tmp/filelist.$$ $BASE/$NAME/updates/$backup_name
+    fi
+
+    rm -f /tmp/filelist.$$
+}
+
+DIRS="digikam
+      imports
+      linked-dirs
+      refile
+      repos
+      SanDisk32
+      vc
+"
+
+if [ ! -d $BASE ]; then
+    echo "Backup drive is not mounted" 1>&2
+    exit 1
+fi
+
+for dir in $DIRS; do
+    echo Backing up $dir
+    do_backup $dir
+done
