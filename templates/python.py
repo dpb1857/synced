f0852157943365c465cdd82fd90740d438f400b3
Don Bennett <dpb@donbennett.org>
2017-02-15 11:28:12 -0800

    Add sample argparse to python template main

--- templates/python.py
+++ templates/python.py
@@ -6,10 +6,20 @@ Module comment
 
 # Find pylint codes at: http://pylint-messages.wikidot.com/all-codes
 # pylint: disable=line-too-long
-# pylint:disable=invalid-name
+# pylint: disable=invalid-name
 
 def main():
-    pass
+    "Main function body"
+    import argparse
+
+    parser = argparse.ArgumentParser(description="myscript")
+    parser.add_argument("values", metavar="values", type=int, nargs="+", help="values")
+    parser.add_argument("--xflag", "-x", action="store_true")
+    parser.add_argument("--count", "-c", type=int)
+    parser.add_argument("--file", "-f")
+
+    args = parser.parse_args()
+    print "args:", args
 
 if __name__ == "__main__":
     main()
