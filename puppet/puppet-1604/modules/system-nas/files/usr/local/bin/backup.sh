367e70c69e894185d5c16f219b810825c51dcbb1
Don Bennett <dpb@donbennett.org>
2017-04-18 09:49:11 -0700

    btrfs cmd moved to /bin in 1604

--- puppet/puppet-1604/modules/system-nas/files/usr/local/bin/backup.sh
+++ puppet/puppet-1604/modules/system-nas/files/usr/local/bin/backup.sh
@@ -3,8 +3,8 @@
 NAME=$1
 
 if [ ! -d /backup/$NAME/current ]; then
-    /sbin/btrfs subvolume create /backup/$NAME
-    /sbin/btrfs subvolume create /backup/$NAME/current
+    btrfs subvolume create /backup/$NAME
+    btrfs subvolume create /backup/$NAME/current
 fi
 
 rsync -av --delete-after /srv/$NAME/ /backup/$NAME/current > /tmp/filelist.$$
@@ -13,7 +13,7 @@ updates=`cat /tmp/filelist.$$ | sed '1d' | head -n -3 | grep -v '/$' | wc -l`
 
 if [ $updates -gt 0 ]; then
     backup_name="/backup/$NAME/`date +%Y-%m-%dT%H:%M`"
-    /sbin/btrfs subvolume snapshot -r /backup/$NAME/current $backup_name
+    btrfs subvolume snapshot -r /backup/$NAME/current $backup_name
 fi
 
 rm -f /tmp/filelist.$$
