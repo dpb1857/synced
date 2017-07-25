3516d9396e912cc4047d0c224cc777032bdf2822
Don Bennett <don@armory.io>
2017-07-24 17:47:41 -0700

    Update mac backup script.

--- bin/mac-backup.sh
+++ bin/mac-backup.sh
@@ -2,9 +2,11 @@
 set -x
 cd $HOME
 tar cvzf mac-secret.tar.gz secret
-mcrypt mac-secret.tar.gz # creates secret.tar.gz.mc
-rsync -av mac-secret.tar.gz.nc linode.donbennett.org:~dpb
-rm -f mac-secret.tar.gz mac-secret.tar.gz.nc
+
+# mcrypt seems busted... :-(
+# mcrypt mac-secret.tar.gz # creates secret.tar.gz.mc
+rsync -av mac-secret.tar.gz linode.donbennett.org:~dpb
+rm -f mac-secret.tar.gz
 
 tar cvzf mac-documents.tar.gz Documents
 rsync -av mac-documents.tar.gz linode.donbennett.org:~dpb
