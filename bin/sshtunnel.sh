b4a9ecb36f6b7751a6221db79e7c68045e901419
Don Bennett <dpb@donbennett.org>
2017-08-31 21:17:31 -0700

    Tunnel updates.

--- bin/sshtunnel.sh
+++ bin/sshtunnel.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
+set -x
+exec < /dev/null
+exec > /home/dpb/sshtunnel.log 2>&1
 
 TUNNEL_HOST=linode.donbennett.org
 TUNNEL_USER=dpb
-# TUNNEL_HOST=newnewlegacynew.prod.quixey.com
-# TUNNEL_USER=don
+TUNNEL_PORT=22022
 
 while true; do
-    ssh $TUNNEL_HOST -l $TUNNEL_USER -R 32022:localhost:22 "while true; do date; sleep 60; done";
+    ssh $TUNNEL_HOST -l $TUNNEL_USER -R ${TUNNEL_PORT}:localhost:22 "while true; do date; sleep 60; done";
     sleep 60;
 done
