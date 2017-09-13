963e7a1a751b39e9d3ba5763f1c9ab9e1d1e2299
Don Bennett <dpb@donbennett.org>
2017-09-13 11:18:16 -0700

    Tunnel the router port also.

--- bin/sshtunnel.sh
+++ bin/sshtunnel.sh
@@ -6,8 +6,9 @@ exec > /home/dpb/sshtunnel.log 2>&1
 TUNNEL_HOST=linode.donbennett.org
 TUNNEL_USER=dpb
 TUNNEL_PORT=22022
+ROUTER_PORT=22080
 
 while true; do
-    ssh $TUNNEL_HOST -l $TUNNEL_USER -R ${TUNNEL_PORT}:localhost:22 "while true; do date; sleep 60; done";
+    ssh $TUNNEL_HOST -l $TUNNEL_USER -R ${TUNNEL_PORT}:localhost:22 -R ${ROUTER_PORT}:192.168.2.1:80 "while true; do date; sleep 60; done";
     sleep 60;
 done
