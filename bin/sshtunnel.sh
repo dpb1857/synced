c93a83048abfaad2b06749df19eaa2ed6f4877d4
Don Bennett <dpb@donbennett.org>
2017-09-13 22:06:17 -0700

    Use a public remote listener

--- bin/sshtunnel.sh
+++ bin/sshtunnel.sh
@@ -6,9 +6,11 @@ exec > /home/dpb/sshtunnel.log 2>&1
 TUNNEL_HOST=linode.donbennett.org
 TUNNEL_USER=dpb
 TUNNEL_PORT=22022
+
+# Note sshd on remote host needs 'GatewayPorts yes' to enable a remote public listener;
 ROUTER_PORT=22080
 
 while true; do
-    ssh $TUNNEL_HOST -l $TUNNEL_USER -R ${TUNNEL_PORT}:localhost:22 -R ${ROUTER_PORT}:192.168.2.1:80 "while true; do date; sleep 60; done";
+    ssh $TUNNEL_HOST -l $TUNNEL_USER -R ${TUNNEL_PORT}:localhost:22 -R 0.0.0.0:${ROUTER_PORT}:192.168.2.1:80 "while true; do date; sleep 60; done";
     sleep 60;
 done
