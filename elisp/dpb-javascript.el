3aef8a9b2dc0f898a43314af309bb7efe2fd0510
Don Bennett <dpb@donbennett.org>
2017-11-30 16:13:27 -0800

    Emacs js tweaks.

--- elisp/dpb-javascript.el
+++ elisp/dpb-javascript.el
@@ -2,15 +2,28 @@
 ;; Javascript/jsx setup;
 
 ;; much taken from http://codewinds.com/blog/2015-04-02-emacs-flycheck-eslint-jsx.html
+;; XXX re-read if we want better linting of our javascript...
 
 
-;; Install js2 mode;
+;; Install js2-mode;
 
 (unless (package-installed-p 'js2-mode)
   (package-refresh-contents)
   (package-install 'js2-mode))
 
 (require 'js2-mode)
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+
+;; Install web-mode;
+
+(unless (package-installed-p 'web-mode)
+  (package-refresh-contents)
+  (package-install 'web-mode))
+
+(require 'web-mode)
+;; from http://cha1tanya.com/2015/06/20/configuring-web-mode-with-jsx.html
+(setq web-mode-content-types-alist
+  '(("jsx" . "\\.js[x]?\\'")))
 
 ;;
 ;; Install json mode;
@@ -30,7 +43,6 @@
 ;; use eslint with web-mode for jsx files
 (flycheck-add-mode 'javascript-eslint 'web-mode)
 
-
-(require 'json-mode)
+;; (require 'json-mode)
 
 (provide 'dpb-javascript)
