56473407a2d67f839cd456f16745cfa023eff0fc
Don Bennett <dpb@donbennett.org>
2017-12-05 08:55:10 -0800

    Cleanup emacs.

--- elisp/dired-fix.el
+++
@@ -1,6 +0,0 @@
-
-(add-hook 'dired-load-hook
-	  (lambda()
-	    (setq dired-use-ls-dired nil)))
-
-(provide 'dired-fix)
--- elisp/dpb-python.el
+++ elisp/dpb-python.el
@@ -1,13 +1,18 @@
 
 ;; Python mode setup;
 
+(unless (package-installed-p 'python-mode)
+  (package-refresh-contents)
+  (package-install 'python-mode))
+
 (require 'python-mode)
 
-(setq auto-mode-alist (cons '("\\.py$" . python-mode) auto-mode-alist))
-(setq interpreter-mode-alist (cons '("python" . python-mode)
-				   interpreter-mode-alist))
+;; (setq auto-mode-alist (cons '("\\.py$" . python-mode) auto-mode-alist))
 
-(autoload 'python-mode "python-mode" "Python editing mode." t)
+;; (setq interpreter-mode-alist (cons '("python" . python-mode)
+;;				   interpreter-mode-alist))
+
+;; (autoload 'python-mode "python-mode" "Python editing mode." t)
 
 ;; lambda-mode;
 ;; (require 'lambda-mode)
