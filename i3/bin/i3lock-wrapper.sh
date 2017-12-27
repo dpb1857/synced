15bf9f212fa41ddeb33d3c122e1f09c0f00b9137
Don Bennett <dpb@donbennett.org>
2017-12-26 23:23:38 -0800

    screensaver tweaks

--- i3/bin/i3lock-wrapper.sh
+++ i3/bin/i3lock-wrapper.sh
@@ -4,7 +4,8 @@ revert() {
     xset dpms 0 0 0
 }
 trap revert HUP INT TERM
-xscreensaver-command -deactivate
+xscreensaver-command -exit
 xset +dpms dpms 30 30 30
 i3lock -n -t -e -i /home/dpb/synced/i3/images/padlock.png
+xscreensaver -no-splash &
 revert
