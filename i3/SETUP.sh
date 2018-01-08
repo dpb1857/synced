5511c0567630a4e7858e4841b837f7211f0916b7
Don Bennett <dpb@donbennett.org>
2018-01-08 10:45:42 -0800

    Tweak

--- i3/SETUP.sh
+++ i3/SETUP.sh
@@ -5,3 +5,5 @@ mkdir -p $HOME/.config/mpv
 ln -s $HOME/synced/i3/config $HOME/.config/i3/config
 ln -s $HOME/synced/i3/i3status.conf $HOME/.i3status.conf
 ln -s $HOME/synced/i3/mpv-config $HOME/.config/mpv/config
+
+cp dot-xscreensaver $HOME/.xscreensaver
