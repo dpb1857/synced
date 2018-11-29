8340afe5286bcf40acc64e19786421dacc8732f8
Don Bennett <dpb@donbennett.org>
2018-11-28 23:53:08 -0800

    fixes.

--- bin/photos/photosBackup.sh
+++ bin/photos/photosBackup.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
-set -x
+
 BASE=/media/dpb/Backup
 NAME=PhotoArchive
+
+if [ ! -d $BASE ]; then
+    echo "Backup drive not mounted." 1>&2
+    exit 1
+fi
+
+if [ ! -d /media/dpb/PhotoArchive ]; then
+    echo "PhotoArchive drive not mounted." 1>&2
+    exit 1
+fi
 
 if [ ! -d $BASE/$NAME/current ]; then
     /bin/btrfs subvolume create $BASE/$NAME
@@ -9,12 +19,15 @@ if [ ! -d $BASE/$NAME/current ]; then
     mkdir $BASE/$NAME/updates
 fi
 
-rsync -av -n --delete-after /media/dpb/PhotoArchive/ $BASE/$NAME/current > /tmp/filelist.$$
+echo "rsync started:" `date`
+rsync -av --delete-after /media/dpb/PhotoArchive/ $BASE/$NAME/current > /tmp/filelist.$$
+echo "rsync complete:" `date`
 updates=`cat /tmp/filelist.$$ | sed '1d' | head -n -3 | grep -v '/$' | wc -l`
 
 if [ $updates -gt 0 ]; then
     backup_name="`date +%Y-%m-%dT%H:%M`"
     /bin/btrfs subvolume snapshot -r $BASE/$NAME/current $BASE/$NAME/$backup_name
+    echo "subvolume creation complete:" `date`
     cp /tmp/filelist.$$ $BASE/$NAME/updates/$backup_name
     cat /tmp/filelist.$$
 fi
