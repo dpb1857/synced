8e2a0169719b278523c4adaefd87f7b557e99837
Don Bennett <dpb@donbennett.org>
2018-12-28 17:36:31 -0800

    Install mypy with flycheck support.

--- elisp/dpb-flycheck.el
+++ elisp/dpb-flycheck.el
@@ -6,4 +6,22 @@
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
 (require 'flycheck)
+
+;; Add mypy to the python checkers;
+;; from https://www.reddit.com/r/emacs/comments/7dbunc/emacs_python_36_type_checking_support_using_mypy/
+
+(flycheck-define-checker
+    python-mypy ""
+    :command ("mypy"
+              "--ignore-missing-imports"
+              "--python-version" "3.6"
+              source-original)
+    :error-patterns
+    ((error line-start (file-name) ":" line ": error:" (message) line-end))
+    :modes python-mode)
+
+(add-to-list 'flycheck-checkers 'python-mypy t)
+(flycheck-add-next-checker 'python-pylint 'python-mypy t)
+
+
 (provide 'dpb-flycheck)
