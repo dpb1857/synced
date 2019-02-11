08d7d7a40bdd6fd458167ae0f06910bd457f48b4
Don Bennett <dpb@donbennett.org>
2019-02-10 22:43:03 -0800

    Notes on clj(s) flycheck; not working yet.

--- elisp/dpb-clj.el
+++ elisp/dpb-clj.el
@@ -11,6 +11,13 @@
   (package-refresh-contents)
   (package-install 'cider))
 
+;; XXX Doesn't quite work; try again later; also, checkout this package:
+;; https://github.com/clojure-emacs/squiggly-clojure
+;;
+;; (unless (package-installed-p 'flycheck-clojure)
+;;   (package-refresh-contents)
+;;   (package-install 'flycheck-clojure))
+
 (require 'cider)
 
 (provide 'dpb-clj)
