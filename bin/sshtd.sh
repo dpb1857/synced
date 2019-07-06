387a0d5e146b882ef6b1c4d29bd39aca2f3914cb
Don Bennett <dpb@donbennett.org>
2019-07-05 23:33:22 -0700

    Script comment updates

--- bin/sshtd.sh
+++ bin/sshtd.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-# To launch at boot, you can put this in the root crontab:
-# @reboot /usr/bin/sshtd
+# Setup:
+# -> create ssh keys for root;
+# -> add the public key to allowed keys on linode:~tunnel
+# -> ssh in the first time to answer question about host key;
+# -> copy this script to <somewhere>
+# -> To launch at boot, you can put this in the root crontab:
+#    @reboot /usr/bin/sshtd
 
 set -x
 exec < /dev/null
@@ -9,7 +14,7 @@ exec > /dev/null 2>&1
 
 TUNNEL_HOST=linode.donbennett.org
 TUNNEL_USER=tunnel
-TUNNEL_PORT=22022
+TUNNEL_PORT=22025 # bigserver
 
 # Note sshd on remote host needs 'GatewayPorts yes' to enable a remote public listener;
 while true; do
