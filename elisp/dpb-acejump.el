c3c518d45a29aa8f1864e8b7a43e6cd0bcbd2b8c
Don Bennett <dpb@donbennett.org>
2021-01-13 15:50:23 -0800

    New binding for acejump: c-`

--- elisp/dpb-acejump.el
+++ elisp/dpb-acejump.el
@@ -7,6 +7,6 @@
   (package-refresh-contents)
   (package-install 'ace-jump-mode))
 
-(define-key global-map (kbd "C-c C-a") 'ace-jump-mode)
+(define-key global-map (kbd "C-`") 'ace-jump-mode)
 
 (provide 'dpb-acejump)
