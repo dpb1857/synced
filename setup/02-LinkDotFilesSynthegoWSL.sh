2fe25466a33c6f98736d4d1cc0cb51503b7d8153
Don Bennett <don.bennett@synthego.com>
2021-08-17 21:29:13 -0700

    Tweak the synthego dot file linker script.

--- setup/02-LinkDotFilesSynthegoWSL.sh
+++ setup/02-LinkDotFilesSynthegoWSL.sh
@@ -11,20 +11,21 @@ fi
 #     ln -s $HOME/synced/dot-files/dot-gitconfig $HOME/.gitconfig
 #     set +x
 # fi
-# 
+#
 # if [ ! -f $HOME/.lein/profiles.clj -a -f $HOME/synced/dot-files/dot-lein-profiles.clj ]; then
 #     set -x
 #     mkdir -p $HOME/.lein
 #     ln -s $HOME/synced/dot-files/dot-lein-profiles.clj $HOME/.lein/profiles.clj
 #     set +x
 # fi
-# 
-# if [ -f $HOME/synced/dot-files/dot-bashrc-dpb ] && ! grep dot-bashrc $HOME/.bashrc>/dev/null 2>&1; then
-#     echo "Hooking dot-bashrc-dpb into .bashrc..." 1>&2
-#     echo ". $HOME/synced/dot-files/dot-bashrc-dpb" >> $HOME/.bashrc
-# fi
-# 
-# if [ -f $HOME/synced/dot-files/dot-profile-dpb ] && ! grep dot-profile-dpb $HOME/.profile>/dev/null 2>&1; then
-#     echo "Hooking dot-profile-dpb into .profile..." 1>&2
-#     echo ". $HOME/synced/dot-files/dot-profile-dpb" >> $HOME/.profile
-# fi
+#
+
+if [ -f $HOME/synced/dot-files/dot-bashrc-dpb ] && ! grep dot-bashrc $HOME/.bashrc>/dev/null 2>&1; then
+    echo "Hooking dot-bashrc-dpb into .bashrc..." 1>&2
+    echo ". $HOME/synced/dot-files/dot-bashrc-dpb" >> $HOME/.bashrc
+fi
+
+if [ -f $HOME/synced/dot-files/dot-profile-dpb ] && ! grep dot-profile-dpb $HOME/.profile>/dev/null 2>&1; then
+    echo "Hooking dot-profile-dpb into .profile..." 1>&2
+    echo ". $HOME/synced/dot-files/dot-profile-dpb" >> $HOME/.profile
+fi
