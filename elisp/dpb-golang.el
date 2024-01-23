fb45e59a31d3546f744c267d9a36e783590d86ae
Don Bennett <dpb@donbennett.org>
2024-01-22 19:58:55 -0800

    Updates.

--- elisp/dpb-golang.el
+++ elisp/dpb-golang.el
@@ -17,28 +17,28 @@
   (package-refresh-contents)
   (package-install 'go-mode))
 
-(unless (package-installed-p 'go-eldoc)
-  (package-refresh-contents)
-  (package-install 'go-eldoc))
+;; (unless (package-installed-p 'go-eldoc)
+;;   (package-refresh-contents)
+;;   (package-install 'go-eldoc))
 
 ;; (unless (package-installed-p 'go-guru)
 ;;   (package-refresh-contents)
 ;;   (package-install 'go-guru))
 
-(unless (package-installed-p 'go-dlv)
-  (package-refresh-contents)
-  (package-install 'go-dlv))
+;; (unless (package-installed-p 'go-dlv)
+;;   (package-refresh-contents)
+;;   (package-install 'go-dlv))
 
 (require 'go-mode)
-(require 'go-eldoc)
+;; (require 'go-eldoc)
 ;; (require 'go-guru)
-(require 'go-dlv)
+;; (require 'go-dlv)
 
 (add-hook 'go-mode-hook
   (lambda()
     (setq tab-width 4)))
 
-(add-hook 'go-mode-hook 'go-eldoc-setup)
+;; (add-hook 'go-mode-hook 'go-eldoc-setup)
 
 (add-hook 'before-save-hook 'gofmt-before-save)
 
