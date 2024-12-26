6a18b7c0f7f92a1b8916c17ee293f70c91eab42e
Don Bennett <dpb@donbennett.org>
2024-12-25 19:58:19 -0800

    base 2404, copied from 2204

---
+++ package-setup/2404/install.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+name=$1
+if [ -z "$name" ]; then
+    name=`hostname`
+fi
+
+if [ ! -f nodes/$name ]; then
+    echo "Could not find package file for host $name." 1>&2
+    exit 1
+fi
+
+sets=`cat nodes/$name|grep -v '^#'`
+echo "Installing the following package sets:"
+fnames=""
+for set in $sets; do
+    echo $set
+    fnames="$fnames modules/$set"
+done
+
+missing=0
+for fname in $fnames; do
+    if [ ! -f $fname ]; then
+        echo "Module $fname is missing." 1>&2
+        missing=1
+        continue
+    fi
+done
+
+if [ $missing -eq 1 ]; then
+    echo "Exiting." 2>&1
+    exit 2
+fi
+sleep 10
+
+debs=`cat $fnames|grep -v '^#'|sort`
+echo
+echo "installing the following debs:"
+for deb in $debs; do
+    echo $deb
+done
+sleep 10
+
+sudo apt-get install $debs
