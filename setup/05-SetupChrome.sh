74335b4278e0108e5f5cebdb2d595303780efbc7
Don Bennett <dpb@donbennett.org>
2025-03-12 16:57:01 -0700

    When installing Chrome, keep linux signing key out of current directory.

--- setup/05-SetupChrome.sh
+++ setup/05-SetupChrome.sh
@@ -9,8 +9,8 @@
 
 
 # Command line: (deb 12, ubuntu 2024)
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub > linux_signing_key.pub
-sudo install -D -o root -g root -m 644 linux_signing_key.pub /etc/apt/keyrings/linux_signing_key.pub
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub > /tmp/linux_signing_key.pub
+sudo install -D -o root -g root -m 644 /tmp/linux_signing_key.pub /etc/apt/keyrings/linux_signing_key.pub
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/linux_signing_key.pub] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
 
 sudo apt update
