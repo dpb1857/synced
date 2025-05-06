743f8b93d75de14726892ebcc38d66657990b2b5
Don Bennett <dpb@donbennett.org>
2025-05-06 13:48:08 -0700

    Update configuration notes.

---
+++ package-setup/setup.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+function common() {
+    apt-get -y install \
+            emacs \
+            make \
+            mg \
+            ntp \
+            ntpdate \
+            rename
+}
+
+function filesystems() {
+    apt-get -y install \
+            ecryptfs-utils \
+            cryptsetup \
+            sshfs \
+            nfs-common \
+            btrfs-progs
+}
+
+function devtools() {
+    apt-get -y install \
+            jq
+    }
+
+function media {
+    apt-get -y install \
+            ubuntu-restricted-extras \
+            digikam \
+            dcraw \
+            exfat-fuse \
+            exiftool \
+            ffmpeg \
+            gimp \
+            gimp-help-en \
+            gscan2pdf \
+            imagemagick \
+            vlc
+    }
+
+function office {
+    apt-get -y install \
+            xournal
+    }
+
+function systemutils {
+    apt-get -y install \
+            baobab \
+            curl \
+            httpie \
+            dconf-editor \
+            gparted \
+            iotop \
+            nmap \
+            sysstat \
+            traceroute \
+            net-tools \
+            ripgrep
+    }
+
+common
+filesystems
+devtools
+media
+office
+systemutils
